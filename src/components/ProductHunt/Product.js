5700d1d2820152d909469ae31ee327bade1f98cd
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-20 02:29:31 +0800

    improved the code of producthunt exercise

--- src/components/ProductHunt/Product.js
+++ src/components/ProductHunt/Product.js
@@ -1,29 +1,35 @@
 import React, {Component} from "react";
 
 class Product extends Component {
+
+    handleUpVote = () => {
+        this.props.onVote(this.props.id)
+    };
+
     render() {
+        const {productImageUrl, votes, description, url, title,submitterAvatarUrl} = this.props;
         return (
             <div className={'item'}>
                 <div className={'image'}>
                     {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-                    <img src={require('./'+this.props.productImageUrl)} alt="some image"/>
+                    <img src={require('./'+productImageUrl)} alt="some image"/>
                 </div>
                 <div className="middle aligned content">
                     <div className="header">
                         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                        <a><i className={'large caret up icon'}/></a>
-                        {this.props.votes}
+                        <a onClick={this.handleUpVote}><i className={'large caret up icon'}/></a>
+                        {votes}
                     </div>
                     <div className="description">
                         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                        <a href={this.props.url}>{this.props.title}</a>
-                        <p>{this.props.description}</p>
+                        <a href={url}>{title}</a>
+                        <p>{description}</p>
                     </div>
                     <div className="extra">
                         {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
                         <span>Submitted by:</span>
                         {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-                        <img src={require('./'+this.props.submitterAvatarUrl)} alt="image person not found"
+                        <img src={require('./'+submitterAvatarUrl)} alt="image person not found"
                              className="ui avatar image"/>
                     </div>
                 </div>
--- src/components/ProductHunt/ProductList.js
+++ src/components/ProductHunt/ProductList.js
@@ -3,20 +3,53 @@ import Product from "./Product";
 
 
 class ProductList extends Component {
-    render() {
+    state = {
+        products: []
+    };
+
+    componentDidMount() {
         // eslint-disable-next-line no-undef
-        const product = Seed.products[0];
+        this.setState({products: Seed.products});
+    }
+
+    handleProductVoteUp = productId =>  {
+        const nextProducts = this.state.products.map((product) => {
+            if (product.id === productId) {
+                return Object.assign({}, product, {
+                    votes: product.votes + 1
+                });
+            } else {
+                return product;
+            }
+        });
+
+        this.setState({
+            products: nextProducts
+        })
+    };
+
+    render() {
+        const products = this.state.products.sort((a,b) => (
+            b.votes - a.votes
+        ));
+
+        const productComponent = products.map(({id, title, description, url, votes, submitterAvatarUrl,productImageUrl}) => (
+                <Product
+                    key={'product-' + id}
+                    id={id}
+                    title={title}
+                    description={description}
+                    url={url}
+                    votes={votes}
+                    submitterAvatarUrl={submitterAvatarUrl}
+                    productImageUrl={productImageUrl}
+                    onVote={this.handleProductVoteUp}
+                />
+
+        ));
         return (
             <div className={"ui unstackable  items"}>
-                <Product
-                    id={product.id}
-                    title={product.title}
-                    description={product.description}
-                    url={product.url}
-                    votes={product.votes}
-                    submitterAvatarUrl={product.submitterAvatarUrl}
-                    productImageUrl={product.productImageUrl}
-                />
+                {productComponent}
             </div>
         )
     }
